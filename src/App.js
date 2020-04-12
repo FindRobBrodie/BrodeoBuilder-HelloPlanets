a110627627d97013164cd344ff29fd54be7abbe2
Rob Brodie <brodiepro@gmail.com>
2020-04-11 21:16:03 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Brodeo Builder - Hello Planets</h1>
+      
+      /*BRO-LOOP:{planets}*/<div>/*BRO:{greeting}*/ /*BRO:{planet}*/!</div>
+/*END-BRO-LOOP:{planets}*/
+    </div>
+  );
+}
